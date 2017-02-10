307728ec78358d990e8ecbbd5e6d9f0a0a54493b
Joseph Khawly <josephkhawly@gmail.com>
2017-02-10 12:02:38 -0500

    new_style: placed hook function and bookmarks in their own files.

---
+++ new_style/bookmarks.js
@@ -0,0 +1,38 @@
+// Place all bookmarks here
+// Format is ['<command>', '<URL>']
+var hookCommands = [
+    'chan',
+    'date',
+    'dice',
+    'reddit',
+    'time',
+    'weather',
+];
+
+var bookmarks = [
+    ['inbox', 'http://www.inbox.google.com/'],
+    ['cal', "http://www.calendar.google.com/"],
+    ['netflix', 'https://netflix.com/'],
+    ['music', 'https://play.google.com/music/listen?hl=en&u=0#/wmp'],
+    ['trello', 'https://trello.com/'],
+]
+
+var tech = [
+    ['github', 'http://www.github.com/'],
+]
+
+var uni = [
+    ['307', 'http://courses.cs.purdue.edu/cs30700:spring17:start'],
+    ['408', "https://www.cs.purdue.edu/homes/suresh/408-Spring2017/index.htm"],
+    ['bb', 'https://mycourses.purdue.edu/'],
+    ['unimail', 'https://outlook.office.com/'],
+    ['mypurdue', 'https://wl.mypurdue.purdue.edu/'],
+]
+
+var social = [
+    ['fb', 'http://www.facebook.com/'],
+    ['twitter', "https://www.twitter.com/"],
+    ['linkedin', 'https://linkedin.com/in/josephkhawly'],
+    ['groupme', 'https://app.groupme.com/'],
+    ['messenger', 'https://messenger.com/'],
+]
